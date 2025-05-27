ccf2cf279607afadac1da8a67fde0f3f10f0dfb3
FAISALAKBARr <faisalzogg022@gmail.com>
2025-05-27 07:13:17 +0700

    gvugjvhg

--- src/services/fileUploadService.ts
+++ src/services/fileUploadService.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const FILE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api/files"
+const FILE_URL = "http://localhost:5000/api/files"
 
 // Upload a file
 export const uploadFile = async (file: File, token: string) => {
