b4c8e2a0981accebadf0410875e11cfdbed72f70
fikriiaabdullah <fikriiaabdul@gmail.com>
2025-06-27 15:45:13 +0700

    ..

--- src/pages/Employee/EmployeeDashboardPage.tsx
+++ src/pages/Employee/EmployeeDashboardPage.tsx
@@ -687,11 +687,11 @@ const EmployeeDashboardPage = () => {
 
       {/* Main Content */}
       <div className="container mx-auto px-4 py-8">
-        {error && (
-          /*<div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+        {/*{error && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
             {error}
-          </div>*/
-        )}
+          </div>
+        )}*/}
 
         {loading ? (
           <Loading fullscreen={true} />
